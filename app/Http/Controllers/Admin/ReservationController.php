130631fa71e12fdc3d2f04d268c2ab36808ee43b
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-05-29 18:04:00 +0630

    Table, Date, Guests

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\TableStatus;
 use App\Models\Reservation;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReservationStoreRequest;
 use App\Models\Table;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
@@ -28,7 +30,9 @@ class ReservationController extends Controller
      */
     public function create()
     {
-        $tables = Table::all();
+        $tables = Table::where('status', TableStatus::Avaliable)->get();
+        // $tables = Table::all();
+        // dd($tables);
         return view('admin.reservations.create', compact('tables'));
     }
 
@@ -40,6 +44,21 @@ class ReservationController extends Controller
      */
     public function store(ReservationStoreRequest $request)
     {
+        $table = Table::findOrFail($request->table_id);
+        if($request->guest_number > $table->guest_number) 
+        {
+            return back()->with('warning', "Please choose the Table based on Guest");
+        }
+        // $request_date = Carbon::parse($request->res_date);
+        
+        foreach ($table->reservations as $res) {
+            // if ($res->res_date->format('Y-m-d') == $request_date->format('Y-m-d')) {
+            //     return back()->with('warning', "This table is reserved for this date");
+            // }
+            if (date('Y-m-d', strtotime($res->res_date)) == date('Y-m-d', strtotime($request->res_date))) {
+                return back()->with('warning', "This table is reserved for this date");
+            }
+        }
         Reservation::create($request->validated());
         return to_route('admin.reservations.index')->with('success', "Reservation created successfully");
     }
