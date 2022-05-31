ebd5280d19780258b380f76f86fbe4c834f0ee45
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-05-31 21:06:28 +0630

    Display Menus and fixed code

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -5,9 +5,7 @@
             <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
                 <img class="w-full h-48" src="{{Storage::url($menu->image)}}" alt="Image"/>
                 <div class="px-6 py-4">
-                    <a href="{{ route('categoories.show', $menu->id) }}">
                         <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">{{ $menu->name }}</h4>
-                    </a>
                     <p class="leading-normal text-gray-700">{{ $menu->description }}</p>
                 </div>
                 <div class="flex items-center justify-between p-4">
