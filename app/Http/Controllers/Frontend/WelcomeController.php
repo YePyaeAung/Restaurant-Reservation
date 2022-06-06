8734785cb8d3998966264c69f0c0decb4c690ed1
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-06-06 23:28:20 +0630

    Cache and Observer

--- app/Http/Controllers/Frontend/WelcomeController.php
+++ app/Http/Controllers/Frontend/WelcomeController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-        $specials = Category::where('name', 'specials')->first();
+        $specials = Cache::rememberForever('specials', function () {
+            // return DB::table('users')->get();
+            return Category::with('menus')->where('name', 'specials')->first();
+        });
+        // $specials = Category::with('menus')->where('name', 'specials')->first();
         return view('welcome', compact('specials'));
     }
     public function thankyou()
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Menu;
+use App\Observers\MenuObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -20,6 +22,9 @@ class EventServiceProvider extends ServiceProvider
         ],
     ];
 
+    protected $observers = [
+        Menu::class => [MenuObserver::class],
+    ];
     /**
      * Register any events for your application.
      *
@@ -27,7 +32,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Menu::observe(MenuObserver::class);
     }
 
     /**
